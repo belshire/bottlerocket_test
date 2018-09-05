0b5805bf42cd8a6cb65970f6aa04129d54974ff1
Blake Elshire <blake.elshire@gmail.com>
2018-09-05 11:12:37 -0500

    Adding base components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantListComponent,
+    RestaurantDetailComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
