ff459accfcc1ae23e0b3999402295581058e7864
Blake Elshire <blake.elshire@gmail.com>
2018-09-05 20:48:19 -0500

    Implementing google map functionality.

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild, AfterContentInit, OnChanges} from '@angular/core';
 
 import { Restaurant } from '../restaurant';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-restaurant-detail',
   templateUrl: './restaurant-detail.component.html',
   styleUrls: ['./restaurant-detail.component.scss']
 })
-export class RestaurantDetailComponent implements OnInit {
+export class RestaurantDetailComponent implements OnInit, AfterContentInit, OnChanges {
+  @ViewChild('gmap') gmapElement: any;
+
   @Input() restaurant: Restaurant;
+  map: google.maps.Map;
+  marker: google.maps.Marker;
 
   constructor() {
   }
@@ -16,4 +21,31 @@ export class RestaurantDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    if (this.restaurant && this.map) {
+      const location = new google.maps.LatLng(this.restaurant.location.lat, this.restaurant.location.lng);
+
+      this.map.setCenter(location);
+
+      if (!this.marker) {
+        this.marker = new google.maps.Marker({
+          position: location,
+          map: this.map,
+          title: this.restaurant.name
+        });
+      } else {
+        this.marker.setPosition(location);
+        this.marker.setTitle(this.restaurant.name);
+      }
+    }
+  }
+
+  ngAfterContentInit() {
+    console.log(this.gmapElement);
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, {
+      center: new google.maps.LatLng(32.7767, 96.7970),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+  }
 }
